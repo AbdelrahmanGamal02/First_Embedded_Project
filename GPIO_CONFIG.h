e3ce620ec4093a9520abe58a7d600f8c7b31ee5e
AbdelrahmanGamal02 <beboelkhateeb@gmail.com>
2023-10-18 22:42:41 +0300

    Configration of GPIO Source code

---
+++ GPIO_CONFIG.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   GPIO_CONFIG.h
+ * Author: abdo
+ *
+ * Created on October 18, 2023, 2:11 PM
+ */
+
+#ifndef GPIO_CONFIG_H
+#define	GPIO_CONFIG_H
+
+#define INTERFACES_ENABLES       0x01
+#define INTERFACES_DISABLES      0x00
+
+#define PIN_CONVEGRATIONS        INTERFACES_ENABLES
+#define PORT_CONVEGRATIONS       INTERFACES_ENABLES
+#endif	/* GPIO_CONFIG_H */
+
