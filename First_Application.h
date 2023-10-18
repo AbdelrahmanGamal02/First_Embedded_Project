6d55ae2619f1c069bfddc5dbd709046126cb8125
AbdelrahmanGamal02 <beboelkhateeb@gmail.com>
2023-10-18 22:40:59 +0300

    Header file of main code

---
+++ First_Application.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   First_Application.h
+ * Author: abdo
+ *
+ * Created on September 19, 2023, 4:51 PM
+ */
+
+#ifndef FIRST_APPLICATION_H
+#define	FIRST_APPLICATION_H
+
+#include "ECUAL_Layer/LED/ECUAL_LED.h"
+
+
+void Application_Initilization(void);
+
+#endif	/* FIRST_APPLICATION_H */
+
